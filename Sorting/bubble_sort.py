628512dc649aa21492d7f0c94fd03fdd873e72c6
mohindar99 <d.mohindar1999@gmail.com>
2024-10-27 14:45:45 -0400

    values of j are changed.

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,9 +1,9 @@
 def bubble_sort(cust_list):
     for i in range(len(cust_list)):
         for j in range(len(cust_list)-1-i):
-            if cust_list[i]>cust_list[i+1]:
-                cust_list[i],cust_list[i+1]=cust_list[i+1],cust_list[i]
+            if cust_list[j]>cust_list[j+1]:
+                cust_list[j],cust_list[j+1]=cust_list[j+1],cust_list[j]
 
     return cust_list
 
-print(bubble_sort([3,2,78,12,15,67,99]))
+print(bubble_sort([98,3,2,78,12,19,15,67,99,6]))
