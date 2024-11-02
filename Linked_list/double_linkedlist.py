d8855f6e2475eea002359a48b7225146d81fc64c
mohindar99 <d.mohindar1999@gmail.com>
2024-11-02 02:31:32 -0400

    completed d_ll

--- Linked_list/double_linkedlist.py
+++ Linked_list/double_linkedlist.py
@@ -1,10 +1,11 @@
-
+# we define a node class to initiate each object to the double linked list
 class Node:
     def __init__(self,value):
         self.value=value
         self.next=None
         self.prev=None
 
+    # Used to print the values of the node instead of the object location by altering the print function
     def __str__(self):
         return str(self.value)
 
@@ -27,6 +28,7 @@ class Double_Linkedlist:
             self.tail=new_node
         self.length+=1
 
+    # used to print all the values by altering print build-in command
     def __str__(self):
         temp_node=self.head
         result=""
@@ -37,6 +39,7 @@ class Double_Linkedlist:
             temp_node=temp_node.next
         return result
 
+    # Adding an element to the beginning of the linked list
     def prepend(self,value):
         new_node=Node(value)
         if self.head is None:
@@ -48,19 +51,21 @@ class Double_Linkedlist:
             self.head=new_node
         self.length+=1
 
-
+    # printing all the node values present in the linked list
     def traverse(self):
         curr_node=self.head
         while curr_node:
             print(curr_node.value)
             curr_node=curr_node.next
 
+    # printing all the node values in the reverse order present in the linked list
     def reverse_traverse(self):
         curr_node=self.tail
         while curr_node:
             print(curr_node)
             curr_node=curr_node.prev
 
+    # search for a specific node and send its index
     def search(self,target):
         curr_node=self.head
         index=0
@@ -71,6 +76,7 @@ class Double_Linkedlist:
             index+=1
         return -1
 
+    # used to get the index of the node
     def get(self,index):
         if index<0 or index>=self.length:
             return None
@@ -84,17 +90,91 @@ class Double_Linkedlist:
                 curr_node=curr_node.prev
         return curr_node
 
+    # Used to set the values of a specific node based on the index value
+    def set_value(self,index,value):
+        node=self.get(index)
+        if node:
+            node.value=value
+            return True
+        return False
 
+    # Used to add a node anywhere in the linked list based on the index of the linked list
+    def insert(self,index,value):
+        if index<0 or index>self.length:
+            print("Index out of range")
+            return
+        new_node=Node(value)
+        if index==0:
+            self.prepend(value)
+            return
+        elif index==self.length:
+            self.append(value)
+            return
+        else:
+            temp_node=self.get(index-1)
+            new_node.next=temp_node.next
+            new_node.prev=temp_node
+            temp_node.next.prev=new_node
+            temp_node.next=new_node
+        self.length+=1
 
+    # Used to remove the first node of the linked list
+    def pop_first(self):
+        popped_node=self.head
+        if self.length==1:
+            self.head=None
+            self.tail=None
+        else:
+            self.head=self.head.next
+            self.head.prev=None
+            popped_node.next=None
+        self.length-=1
+        return popped_node
+
+    # used to remove the last node of the linked list
+    def pop(self):
+        popped_node=self.tail
+        if self.tail==1:
+            self.head=None
+            self.tail=None
+
+        else:
+            self.tail=self.tail.prev
+            self.tail.next=None
+            popped_node.prev=None
+        self.length-=1
+        return popped_node
+
+    #Used to remove any node from the linked list based on the index value
+    def remove(self,index):
+        if index<0 or index>=self.length:
+            return None
+        if index==0:
+            return self.pop_first()
+        if index==self.length-1:
+            return self.pop()
+        popped_node=self.get(index)
+        popped_node.prev.next=popped_node.next
+        popped_node.next.prev=popped_node.prev
+        popped_node.next=None
+        popped_node.prev=None
+        self.length-=1
+        return popped_node
 
 doublelinkedlist1=Double_Linkedlist()
 doublelinkedlist1.append(10)
 doublelinkedlist1.append(20)
 doublelinkedlist1.append(30)
-doublelinkedlist1.prepend(60)
+doublelinkedlist1.append(40)
+doublelinkedlist1.append(50)
+doublelinkedlist1.append(60)
 
+doublelinkedlist1.insert(5,660)
+doublelinkedlist1.set_value(0,100)
+doublelinkedlist1.pop()
+doublelinkedlist1.remove(4)
 print(doublelinkedlist1)
-print(doublelinkedlist1.get(1))
+
 
 
 
