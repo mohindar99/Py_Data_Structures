630963c58154af29e7564e181976eee8bd2c3b44
mohindar99 <d.mohindar1999@gmail.com>
2024-11-14 01:02:55 -0500

    AVL tress completed

--- Tree/AVL_Tree/AVL_trees.py
+++ Tree/AVL_Tree/AVL_trees.py
@@ -91,9 +91,55 @@ def insertNode(rootNode,nodeValue):
     return rootNode
 
 
+def getminValue(rootNode):
+    if rootNode is None or rootNode.leftchild is None:
+        return rootNode
+    return getminValue(rootNode.leftchild)
+
+def deleteNode(rootNode,nodevalue):
+    if rootNode is None:
+        return rootNode
+    elif nodevalue<rootNode.data:
+        rootNode.leftchild=deleteNode(rootNode.leftchild,nodevalue)
+    elif nodevalue>rootNode.data:
+        rootNode.rightchild = deleteNode(rootNode.rightchild, nodevalue)
+    else:
+        if rootNode.leftchild is None:
+            temp=rootNode.rightchild
+            rootNode=None
+            return temp
+        elif rootNode.rightchild is None:
+            temp=rootNode.leftchild
+            rootNode=None
+            return temp
+        temp=getminValue(rootNode.rightchild)
+        rootNode.data=temp.data
+        rootNode.rightchild=deleteNode(rootNode.rightchild,temp.data)
+    rootNode.height=1+max(getHeight(rootNode.leftchild),getHeight(rootNode.rightchild))
+    balance=getBalance(rootNode)
+    if balance>1 and getBalance(rootNode.leftchild)>=0:
+        return rightRotate(rootNode)
+    if balance<-1 and getBalance(rootNode.righthchild)<=0:
+        return leftRotate(rootNode)
+    if balance>1 and getBalance(rootNode.leftchild)<0:
+        rootNode.leftchild=leftRotate(rootNode.leftchild)
+        return rightRotate(rootNode)
+    if balance<-1 and getBalance(rootNode.righchild)>0:
+        rootNode.rightchild = rightRotate(rootNode.rightchild)
+        return leftRotate(rootNode)
+    return rootNode
+
+def deleteAVL(rootNode):
+    rootNode.data=None
+    rootNode.leftchild=None
+    rootNode.rightchild=None
+    return "The AVL tree had been successfully deleted"
+
 custAVL = AVLTree(5)
-insertNode(custAVL,10)
-insertNode(custAVL,4)
+custAVL=insertNode(custAVL,10)
+custAVL=insertNode(custAVL,15)
+custAVL=insertNode(custAVL,20)
+custAVL=deleteNode(custAVL,15)
 
 levelorderTraversal(custAVL)
 
